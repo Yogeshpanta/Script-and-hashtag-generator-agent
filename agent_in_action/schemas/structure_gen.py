bb682fbdc5aff6d207b1ead9b36f1e84a99341c7
yogeshpanta <yogeshpanta33@gmail.com>
2025-05-16 08:45:08 +0545

    update file to break into structure

--- agent_in_action/schemas/structure_gen.py
+++ agent_in_action/schemas/structure_gen.py
@@ -1,18 +1,17 @@
-from pydantic import BaseModel
-from typing import Optional, List
+from typing import Optional, List, TypedDict
 
-
-class UserInput(BaseModel):
+class UserInput(TypedDict):
     """class where a user input are given"""
     campaign_type:str
     product:list[str]
     rough_theme:str
 
-class StructureBreaker(BaseModel):
+
+class StructureBreaker(TypedDict):
     """This is the model where output is generated"""
     structured_data:str
 
-class UserPromptBreakdown(BaseModel):
+class UserPromptBreakdown(TypedDict):
     """Breakdown the user prompt into below keys and variables"""
     campaign_type:Optional[str] 
     products:Optional[list[str]]

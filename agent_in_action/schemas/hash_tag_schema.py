794c790d50235885df6f2a71a2fa60089d3520f9
yogeshpanta <yogeshpanta33@gmail.com>
2025-05-16 08:43:48 +0545

    update schema file

--- agent_in_action/schemas/hash_tag_schema.py
+++ agent_in_action/schemas/hash_tag_schema.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
-from typing import List
+from typing import List, TypedDict
 
-class HashInput(BaseModel):
+class HashInput(TypedDict):
     input_list:str
 
-class HashTagOutput(BaseModel):
+class HashTagOutput(TypedDict):
     hastag: List[str]
+    

804467be05eb04c0b6cd5f7563a981d629b9f16d
yogeshpanta <yogeshpanta33@gmail.com>
2025-05-22 13:27:27 +0545

    reformatted

--- agent_in_action/services/trend_analysis.py
+++ agent_in_action/services/trend_analysis.py
@@ -3,40 +3,17 @@ from agent_in_action.schemas.overall_state import AgentState
 from langchain.schema import AIMessage
 import logging
 import pycountry
+from agent_in_action.configs.logging_config import setup_logging
 # from langchain_core.tools import tool
 
 import os
 from dotenv import load_dotenv
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
 load_dotenv()
 
-# class TrendAnalysis:
-#     def __init__(self, keywords:str, host_lang:str, geo_location:str):
-#         self.keywords = keywords
-#         self.host_lang = host_lang
-#         self.geo_location = geo_location
-#         self.api_key = os.getenv("SERPAPI_KEY")
-
-#     def __str__(self):
-#         return "Analysing the trends"
-    
-#     def trending_keyword_generator(self):
-#         """Generates the top trending keywords based on products and title"""
-#         client = serpapi.Client(api_key=self.api_key)
-#         search = client.search(
-#             engine="google",
-#             q= self.keywords,
-#             hl= self.host_lang,
-#             gl=self.geo_location
-#         )
-
-#         organic_results = search.get("organic_results", [])
-#         # snippets = []
-#         # for results in organic_results:
-#         #     snippet = results.get("snippet")
-#         #     snippets.append(snippet)
-#         snippets = [item.get("snippet") for item in organic_results if item.get("snippet")]
-
-#         return " ,".join([i for i in snippets])
 
 def get_country_code(country_name):
     try:
@@ -47,41 +24,41 @@ def get_country_code(country_name):
     except LookupError:
         return None
 
+
 def get_language_code(language_name):
     try:
         language = pycountry.languages.get(name=language_name)
         if language is None:
             language = pycountry.languages.search_fuzzy(language_name)[0]
         # Some languages do not have alpha_2; fallback to language alpha_2 or first part of alpha_3
-        if hasattr(language, 'alpha_2'):
+        if hasattr(language, "alpha_2"):
             return language.alpha_2.lower()  # hl param lowercase
         else:
             return language.alpha_3[:2].lower()
     except LookupError:
         return None
-    
 
 
-def trending_keyword_generator(state:AgentState)->AgentState:
+def trending_keyword_generator(state: AgentState) -> AgentState:
     """
-    Extracts and generates trending keywords related to the product(s) provided 
+    Extracts and generates trending keywords related to the product(s) provided
     in the structured data, and updates the agent state with those keywords.
 
-    This function uses the SerpAPI to perform a Google search based on the product 
-    names, language, and location specified in the `structured_data` of the agent state. 
-    It collects the snippets from the top organic search results, which are assumed to 
+    This function uses the SerpAPI to perform a Google search based on the product
+    names, language, and location specified in the `structured_data` of the agent state.
+    It collects the snippets from the top organic search results, which are assumed to
     contain popular and contextually relevant phrases or keywords.
 
-    These extracted keywords are combined into a single comma-separated string and stored 
-    in the `trendy_keywords` field of the `AgentState`. The function also appends an 
+    These extracted keywords are combined into a single comma-separated string and stored
+    in the `trendy_keywords` field of the `AgentState`. The function also appends an
     AIMessage to the state’s `messages` to log the generated keywords.
 
     Args:
-        state (AgentState): The current state of the agent, including structured user input 
+        state (AgentState): The current state of the agent, including structured user input
                             with products, language, and location.
 
     Returns:
-        AgentState: The updated state with extracted trending keywords and an appended message 
+        AgentState: The updated state with extracted trending keywords and an appended message
                     containing those keywords.
 
     Raises:
@@ -101,23 +78,23 @@ def trending_keyword_generator(state:AgentState)->AgentState:
 
     client = serpapi.Client(api_key=os.getenv("SERPAPI_KEY"))
     search = client.search(
-                engine = "google",
-                q = title,
-                # hl = required_keyword.get("language", "en"),
-                # # gl = required_keyword.get("location","us"),
-                hl = "en",
-                gl = "us"
-                # hl = lang_code,
-                # gl = country_code
-        )
-    logging.info("trending words is being extracted")
+        engine="google",
+        q=title,
+        # hl = required_keyword.get("language", "en"),
+        # # gl = required_keyword.get("location","us"),
+        hl="en",
+        gl="us",
+        # hl = lang_code,
+        # gl = country_code
+    )
+    logger.info("trending words is being extracted")
 
     organic_results = search.get("organic_results", [])
     snippets = [item.get("snippet") for item in organic_results if item.get("snippet")]
     state["trendy_keywords"] = " ,".join([i for i in snippets])
     print(state["trendy_keywords"])
-    state["messages"].append(AIMessage(content= f"trendy keyword generated: {state['trendy_keywords']}"))
-    logging.info("trending words extracted")
+    state["messages"].append(
+        AIMessage(content=f"trendy keyword generated: {state['trendy_keywords']}")
+    )
+    logger.info("trending words extracted")
     return state
-
-

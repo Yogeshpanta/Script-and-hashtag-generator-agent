05e7cac5a5d51d4c4ebf7568353247c6412b9329
yogeshpanta <yogeshpanta33@gmail.com>
2025-05-22 13:30:15 +0545

    formatted

--- agent_in_action/schemas/overall_state.py
+++ agent_in_action/schemas/overall_state.py
@@ -3,6 +3,7 @@ from typing_extensions import TypedDict
 from pydantic import BaseModel
 from typing import Optional, Dict, Any, List, Annotated, Literal
 from langchain.schema import HumanMessage, AIMessage
+
 # from agent_in_action.schemas.structure_gen import UserInput, UserPromptBreakdown
 from langgraph.graph import END
 
@@ -13,19 +14,25 @@ from langgraph.graph import END
 class UserInput(BaseModel):
     user_prompt: str
 
+
 class GenerateResponse(BaseModel):
     generated_script: Optional[str] = None
     hashtags: Optional[List[str]] = None
 
+
 class StepGeneration(TypedDict):
     """class which generates objectives and steps"""
 
+
 class GenerateScript(TypedDict):
     """This is the model where output is generated"""
-    script:Dict[str, Any]
+
+    script: Dict[str, Any]
+
 
 class AgentState(TypedDict):
     """Main state for the entire workflow"""
+
     # Original user input
     user_input: Optional[UserInput]
     # Structured data from user input
@@ -35,10 +42,16 @@ class AgentState(TypedDict):
     # Script generated based on structured data
     generated_script: Optional[str]
     # inputs for hashtag generatiion
-    trendy_keywords:Optional[str]
+    trendy_keywords: Optional[str]
     # Hashtags generated for the campaign
     hashtags: Optional[List[str]]
     # The messages passed between components
     messages: List[Annotated[HumanMessage | AIMessage, "messages"]]
     # The current agent that is active
-    current_agent: Literal["supervisor_node", "script_generator", "trending_keywords_generator", "hash_tag_generator", END]
+    current_agent: Literal[
+        "supervisor_node",
+        "script_generator",
+        "trending_keywords_generator",
+        "hash_tag_generator",
+        END,
+    ]
--- agent_in_action/schemas/structure_gen.py
+++ agent_in_action/schemas/structure_gen.py
@@ -1,21 +1,26 @@
-from typing import Optional, List, TypedDict
+from typing import Optional, TypedDict
+
 
 class UserInput(TypedDict):
     """class where a user input are given"""
-    campaign_type:str
-    product:list[str]
-    rough_theme:str
+
+    campaign_type: str
+    product: list[str]
+    rough_theme: str
 
 
 class StructureBreaker(TypedDict):
     """This is the model where output is generated"""
-    structured_data:str
+
+    structured_data: str
+
 
 class UserPromptBreakdown(TypedDict):
     """Breakdown the user prompt into below keys and variables"""
-    campaign_type:Optional[str] 
-    products:Optional[list[str]]
-    location:Optional[str]
-    language:Optional[str]
-    Tone :Optional[str] 
-    end_customer :Optional[str]
+
+    campaign_type: Optional[str]
+    products: Optional[list[str]]
+    location: Optional[str]
+    language: Optional[str]
+    Tone: Optional[str]
+    end_customer: Optional[str]

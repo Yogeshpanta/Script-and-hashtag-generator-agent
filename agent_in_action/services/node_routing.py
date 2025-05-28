ac4db789ebe92f35be71c1e7df7e6048e0ee1bf8
yogeshpanta <yogeshpanta33@gmail.com>
2025-05-28 08:37:38 +0545

    update conditional node file

--- agent_in_action/services/node_routing.py
+++ agent_in_action/services/node_routing.py
@@ -2,41 +2,6 @@ from agent_in_action.schemas.overall_state import AgentState
 from langgraph.graph import END
 
 
-# def node_selector(state:AgentState):
-#     """Selects node based on the condition"""
-#     mode = state['structured_data']
-#     mode_definer = mode.get("mode")
-#     if mode_definer == "script":
-#         return "script_generator"
-
-#     if mode_definer == "hashtag":
-#         return "hashtag_generator"
-
-#     else:
-#         return END
-
-# def node_selector(state: AgentState):
-#     """Enhanced node selector with progress tracking"""
-#     structured_data = state.get("structured_data", {})
-#     mode = structured_data.get("mode")
-
-#     # Flow control flags
-#     trend_done = "trendy_keywords" in state
-#     hashtags_done = "hashtags" in state
-
-#     if mode == "script":
-#         return "script_generator"
-
-#     if mode == "hashtag":
-#         if not trend_done:  # First cycle: LLM -> trend_analyser
-#             return "trend_analyser"
-#         elif not hashtags_done:  # Second cycle: LLM -> hashtag_generator
-#             return "hashtag_generator"
-
-#     # Final cycle: return to LLM for completion
-#     return END
-
-
 def node_selector(state: AgentState):
     """Enhanced node selector with progress tracking"""
     structured_data = state.get("structured_data", {})

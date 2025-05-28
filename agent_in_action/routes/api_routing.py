0f2eb5a6864b682132627764711c6aa735bc4723
yogeshpanta <yogeshpanta33@gmail.com>
2025-05-28 08:35:27 +0545

    update api routing file

--- agent_in_action/routes/api_routing.py
+++ agent_in_action/routes/api_routing.py
@@ -6,9 +6,19 @@ from agent_in_action.schemas.overall_state import (
 
 from fastapi import APIRouter
 
+from agent_in_action.services.hash_tag_gen import hash_tag_generator
 from agent_in_action.services.sturct_break import structure_generator
 from agent_in_action.services.base_class import supervisor_node
+from agent_in_action.services.script_gen import script_generator
+from agent_in_action.services.trend_analysis import trending_keyword_generator
+from agent_in_action.configs.logging_config import setup_logging
+import logging
 import json
+
+setup_logging()
+
+logger = logging.getLogger(__name__)
+
 
 router = APIRouter()
 
@@ -37,7 +47,7 @@ def initialize_state(user_prompt: str) -> AgentState:
 @router.post("/script_hashtag", response_model=GenerateResponse)
 def script_hashtag_generator(request: UserInput):
     state = initialize_state(request.user_prompt)
-
+    logger.info("Routed to fast api")
     # structuring the input
     state = structure_generator(state)
 
@@ -58,5 +68,5 @@ def script_hashtag_generator(request: UserInput):
     # If hashtags is a string, split to list
     # if hashtags and isinstance(hashtags, str):
     #     hashtags = [tag.strip() for tag in hashtags.split(",") if tag.strip()]
-
+    logger.info("returning hastags and scripts")
     return GenerateResponse(generated_script=script, hashtags=hashtags)

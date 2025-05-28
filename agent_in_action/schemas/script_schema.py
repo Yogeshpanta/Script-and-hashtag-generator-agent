a3186c6cdd56b90ae88b06e5005b40180617229a
yogeshpanta <yogeshpanta33@gmail.com>
2025-05-28 08:36:51 +0545

    update script_schema

--- agent_in_action/schemas/script_schema.py
+++ agent_in_action/schemas/script_schema.py
@@ -3,7 +3,9 @@ from typing import TypedDict
 
 class GenerateScript(TypedDict):
     """This is the model where output is generated"""
-    script:str
+
+    script: str
+
 
 # class GenerateScript(BaseModel):
 #     """This is the model where output is generated"""
